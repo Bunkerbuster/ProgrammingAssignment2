495be54c350153c5fddf8f51ceaebc4c3c18e1df
Martijn Diederen <martijn.diederen@gmail.com>
2015-10-25 16:41:51 +0100

    Assignment: Caching the Inverse of a Matrix
    makeCacheMatrix: This function creates a special "matrix" object that
    can cache its inverse.
    
    cacheSolve: This function computes the inverse of the special "matrix"
    returned by makeCacheMatrix above. If the inverse has already been
    calculated (and the matrix has not changed), then the cachesolve should
    retrieve the inverse from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## The function "makeCacheMatrix" creates a special "matrix" object, that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Set Variable "SpecialMatrix" to NULL
+  SpecialMatrix <- NULL
+  
+  # Setter 
+  # Set the Value of the matrix
+  set <- function(y)
+  {
+    x <<- y
+    SpecialMatrix <<- NULL
+  }
+  
+  # Getter
+  # Get the value of the matrix
+  get <- function() x
+  
+  # Set the inverse of the SpecialMatrix
+  # using the function solve to computing the inverse of a square matrix
+  setInverse <- function(i) SpecialMatrix <<- solve(x)
+  
+  # Get the inverse of the SpecialMatrix
+  getInverse <- function() SpecialMatrix
+  
+  # The list containing the function
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## get the inverse of the specialmatrix
+  SpecialMatrix <- x$getInverse()
+  
+  # if the SpecialMatrix is not null then return the specialMatrix (because it has already been computed and cached)
+  if(!is.null(SpecialMatrix))
+  {
+    return(SpecialMatrix)
+  }
+  
+  # because the specialMatrix is not yet computed we now can use the function Solve to compute the inverse of a square matrix
+  SpecialMatrix <- solve(x$get())
+  
+  # The computed result we cache using the setinverse
+  x$setInverse(SpecialMatrix)
+  
+  # return the SpecialMatrix
+  SpecialMatrix
 }
